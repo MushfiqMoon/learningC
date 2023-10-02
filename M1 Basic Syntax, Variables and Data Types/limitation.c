6e3d4243024d6ece3dc4e514646e7a37b2feed6f
MushfiqMoon <mushfiq.moon013@google.com>
2023-10-02 18:28:25 +0600

    adding some comments

--- M1 Basic Syntax, Variables and Data Types/limitation.c
+++ M1 Basic Syntax, Variables and Data Types/limitation.c
@@ -1,4 +1,16 @@
 #include <stdio.h>
+
+/*
+
+int             ( 4 byte )  | %d for pringint
+float           ( 4 byte )  | %f for pringint
+char            ( 1 byte )  | %c for pringint
+
+long long int   ( 8 byte )  | %lld for pringint
+double          ( 8 byte )  | %lf for pringint
+
+*/
+
 int main(){
 
     // // long integer // input : 123456789123456
