4e002dd9cf0a0a2bdffe5934d5ce53b35fdd8db7
MushfiqMoon <mushfiq.moon013@google.com>
2023-10-07 23:37:17 +0600

    2 problem solved

---
+++ M5 Conditional Statements Recap/welcomeLoop.c
@@ -0,0 +1,33 @@
+/*
+Given two numbers A and B. Print "Yes" if A is greater than or equal to B. Otherwise print "No".
+
+Input
+Only one line containing two numbers A and B (0  ≤  A, B  ≤  100).
+
+Output
+Print "Yes" or "No" according to the statement.
+
+inputCopy
+10 9
+outputCopy
+Yes
+
+inputCopy
+5 7
+outputCopy
+No
+*/
+
+#include <stdio.h>
+
+int main(){
+
+    int a, b;
+
+    scanf("%d %d", &a, &b);
+
+    if (a>= b) printf("Yes\n");
+    else printf("No\n");
+
+    return 0;
+}
