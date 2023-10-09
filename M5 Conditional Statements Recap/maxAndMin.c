8aa0bbb589634b0a17df432fbd82ddc826c6589a
MushfiqMoon <mushfiq.moon013@google.com>
2023-10-09 22:58:56 +0600

    add problem text

--- M5 Conditional Statements Recap/maxAndMin.c
+++ M5 Conditional Statements Recap/maxAndMin.c
@@ -1,3 +1,22 @@
+/*
+Given 3 numbers A, B and C, Print the minimum and the maximum numbers.
+
+Input
+Only one line containing 3 numbers A, B and C ( - 105 ≤ A, B, C ≤ 105)
+
+Output
+Print the minimum number followed by a single space then print the maximum number.
+
+input
+1 2 3
+output
+1 3
+
+input
+-1 -2 -3
+output
+-3 -1
+*/
 #include <stdio.h>
 
 int main()
