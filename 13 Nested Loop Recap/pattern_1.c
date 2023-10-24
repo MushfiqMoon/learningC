6d8369eb0b5d51c04a053edfaf6ad8556b12d9aa
MushfiqMoon <mushfiq.moon013@google.com>
2023-10-24 23:04:48 +0600

    practice problem done

---
+++ 13 Nested Loop Recap/pattern_1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(){
+
+    int count;
+    scanf("%d", &count);
+
+    for (int i = count; i >= 1; i--)
+    {
+
+        for (int j = 0; j < i; j++)
+        {
+            printf("&");
+        }
+        printf("\n");
+        
+    }
+    
+    return 0;
+}
